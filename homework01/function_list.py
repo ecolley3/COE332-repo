6774ba998f332a183605cc8bc061e338ddf54a22
Joe Allen <ethancolley@utexas.edu>
2022-01-29 14:51:40 -0600

    add function_list

---
+++ homework01/function_list.py
@@ -0,0 +1,10 @@
+def print_ts(mylist):
+    for x in mylist:
+        if(x[0] == 't'):
+            print(x)
+
+list1 = ['circle', 'heart', 'triangle', 'square']
+list2 = ['one','two','three','four']
+
+print_ts(list1)
+print_ts(list2)
