fdc54cdb37bc0144b47f3a7e8ad6b1e09e2f36de
Ethan Colley <ethancolley@utexas.edu>
2022-04-04 20:17:48 -0500

    updated homework 5

--- homework05/app.py
+++ homework05/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 meteor_data={}
 
 @app.route('/data',methods=['POST'])
-    def load_data():
+def load_data():
     """
     Loads data from JSON file and reports when the data is read.
     """
@@ -17,7 +17,7 @@ meteor_data={}
     with open('ML_Data_Sample.json' , 'r') as f:
         ml_data =json.load(f)
     for d in ml_data['meteorite_landings']:
-        rd.set(d['id'],json.dumps(d))
+        n.set(d['id'],json.dumps(d))
     return f'Data was loaded\n'
 
 @app.route('/data', methods=['GET'])
@@ -31,5 +31,5 @@ def name():
     logging.info("Finding site names")
     list_of_data = []
     for i in range(10001, 10301, 1):
-        list_of_data.append(json.loads(rd.get(i)))
+        list_of_data.append(json.loads(n.get(i)))
     return json.dumps(list_of_data, indent=4)+'\n'
